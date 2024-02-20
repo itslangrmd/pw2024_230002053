6efe26a1ba197e457a7f6c7d6b436b0765b6dd0c
unknown <achlulgilangramadhan@gmail.com>
2024-02-20 13:22:19 +0700

    first commit

---
+++ tugas1.php
@@ -0,0 +1,21 @@
+<?php
+$topi = '"Topi Saya Bundar,';
+$bundar = 'Bundar Topi Saya."';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tugas1</title>
+</head>
+<body>
+    <h1>
+    <?php 
+    echo "<em> $topi " . $bundar . "</em>";
+    ?>
+    </h1>
+   
+</body>
+</html>
---
+++ tugas2.php
@@ -0,0 +1,29 @@
+<?php
+
+// String untuk menyimpan banyak baris
+$baris = "
+    Aku  adalah angka  <b>52</b><br>
+    Jika aku di kali 5 maka aku sekarang  <b>260</b><br>
+    jika aku di bagi 2 maka alu sekarang <b>180</b> <br>
+    jika aku di tambah 75 maka alu sekarang <b>255</b> <br>
+    jika dikurang 20 maka aku sekarang <b>235</b> 
+";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tugas2</title>
+</head>
+<body>
+<?php
+
+
+echo $baris;
+
+?>
+
+</body>
+</html>
