487db11e50b85f82104264c900fceab7c215d8ca
unknown <achlulgilangramadhan@gmail.com>
2024-03-03 17:35:43 +0700

    tugas 3

--- tugas2.php
+++ tugas2.php
@@ -2,11 +2,11 @@
 
 // String untuk menyimpan banyak baris
 $baris = "
-    Aku  adalah angka  <b>52</b><br>
-    Jika aku di kali 5 maka aku sekarang  <b>260</b><br>
-    jika aku di bagi 2 maka alu sekarang <b>180</b> <br>
-    jika aku di tambah 75 maka alu sekarang <b>255</b> <br>
-    jika dikurang 20 maka aku sekarang <b>235</b> 
+    Aku  adalah angka  <b>53</b><br>
+    Jika aku di kali 5 maka aku sekarang  <b>265</b><br>
+    jika aku di bagi 2 maka aku sekarang <b>182.5</b> <br>
+    jika aku di tambah 75 maka aku sekarang <b>257.5</b> <br>
+    jika dikurang 20 maka aku sekarang <b>237.5</b> 
 ";
 
 ?>
